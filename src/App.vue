4868ae30b3b42c6bd00a97138e3b30340889e374
zakiyul <zakiyul.py@gmail.com>
2020-09-23 21:45:11 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <add-blog></add-blog>
+</template>
+
+<script>
+import AddBlog from "./components/addBlog";
+
+export default {
+  components: {
+    "add-blog": AddBlog,
+  },
+};
+</script>
+
+<style>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>

5a69bc740f2e140c6d6963c3a669e9ad85e5410c
zakiyul <zakiyul.py@gmail.com>
2020-09-24 21:13:10 +0800

    custom search filter

--- src/components/showBlog.vue
+++ src/components/showBlog.vue
@@ -1,7 +1,8 @@
 <template>
-  <div v-theme:column="'narrow'" id="show-blogs">
+  <div id="show-blogs">
     <h1>All Blog Articles</h1>
-    <div class="single-blog" v-for="blog in blogs" :key="blog.id">
+    <input type="text" v-model="search" placeholder="search blog.." />
+    <div class="single-blog" v-for="blog in filteredBlogs" :key="blog.id">
       <h2 v-rainbow>{{ blog.title | uppercase }}</h2>
       <article>{{ blog.body | snippet }}</article>
     </div>
@@ -12,12 +13,20 @@ export default {
   data() {
     return {
       blogs: [],
+      search: "",
     };
   },
   created() {
     this.$http
       .get("https://jsonplaceholder.typicode.com/posts")
       .then((res) => (this.blogs = res.body));
+  },
+  computed: {
+    filteredBlogs: function () {
+      return this.blogs.filter((blog) => {
+        return blog.title.match(this.search);
+      });
+    },
   },
 };
 </script>
